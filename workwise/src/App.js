88d7e23a22076132b966df0d4936ef8c0519ab6d
atharva-gadekar <atharvagadekar@gmail.com>
2022-12-13 13:28:28 +0530

    Added Starter Code

---
+++ workwise/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+		<div className="App">
+			<div className="msg">
+				Well done! You have successfully cloned and run this project. Do the
+				following now :
+			</div>
+			<ul className="steps">
+				<li>Install Tailwind using npm in this project following the documentation on tailwind's official site </li>
+        <li>Change all this text on this website to "Installed Tailwind" And only use tailwind to write its CSS</li>
+			</ul>
+		</div>
+	);
+}
+
+export default App;
