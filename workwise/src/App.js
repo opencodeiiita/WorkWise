dfe12117c271f8dbe70e2ad6a154ab1a234f8c3a
Ananya <goyalananya444@gmail.com>
2022-12-18 01:58:39 +0530

    Completed-Task

--- workwise/src/App.js
+++ workwise/src/App.js
@@ -4,14 +4,9 @@ import './App.css';
 function App() {
   return (
 		<div className="App">
-			<div className="msg">
-				Well done! You have successfully cloned and run this project. Do the
-				following now :
+			<div className="msg  text-bold text-red-800">
+				Completed this issue by installing tailwind
 			</div>
-			<ul className="steps">
-				<li>Install Tailwind using npm in this project following the documentation on tailwind's official site </li>
-        <li>Change all this text on this website to "Installed Tailwind" And only use tailwind to write its CSS</li>
-			</ul>
 		</div>
 	); 
 }
