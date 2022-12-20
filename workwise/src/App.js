3f990050b8d4fc81622fd3eaaf237716e606f053
Shubhankit-Pandey <97404864+Shubhankit-Pandey@users.noreply.github.com>
2022-12-20 03:54:48 -0800

    Updated time.js (#15)
    * Created time.js and displayed the time
    
    * Created time.js
    
    * Requested Changes done

--- workwise/src/App.js
+++ workwise/src/App.js
@@ -1,11 +1,18 @@
-import homepage from "./src/pages/homepage";
+import homepage from "./pages/homepage";
 import './App.css';
+import Time from './components/Time'
 
 function App() {
   return (
-		<>
-		<homepage/>
-	</>
+		<div className="App">
+			<div className="msg  text-bold text-red-800">
+				<Time/>
+			</div>
+			<div>
+			<homepage/>
+			</div>
+		</div>
+
 	); 
 }
 
