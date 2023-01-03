0fb9d26e02d64f87e722269f0c6d15600ba85ca3
Kali-Decoder <neerajchoubisa876@gmail.com>
2023-01-03 09:58:50 +0530

    Adding Dashboard

--- workwise/src/App.js
+++ workwise/src/App.js
@@ -1,11 +1,16 @@
 import { Router, Switch, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/homepage.js";
+import Header from "./components/Header.js";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
       </Routes>
+      <Routes>
+        <Route path="/dashboard" element={<Header/>} />
+      </Routes>
+      
     </>
   );
 }
