b725846acda327c64fd309e7445c0fa0799ca3a2
Sanyam07 <agrawalsanyam07@gmail.com>
2023-01-07 14:05:57 +0530

    cards implemented

--- workwise/src/components/Card.js
+++ workwise/src/components/Card.js
@@ -5,43 +5,46 @@ import dayjs from "dayjs";
 function Card(props){
     const card = props.card
     const dateFormat = 'YYYY-MM-DD';
+    const dateFormat2 = 'MMM DD, YYYY';
     return(
-        <div className="w-64 m-4 h-max bg-gray-50 rounded-lg overflow-hidden">
-            <div className="ml-4 mt-4 mb-2">
-                {card.category==='Research' &&
-                    <span class="inline-block bg-blue-500 rounded-full px-3 py-1 text-sm font-semibold text-gray-50 mr-2 mb-2">{card.category}</span>
-                }
-                {card.category==='Content' &&
-                    <span class="inline-block bg-amber-500 rounded-full px-3 py-1 text-sm font-semibold text-gray-50 mr-2 mb-2">{card.category}</span>
-                }
-                {card.category==='Design' &&
-                    <span class="inline-block bg-violet-700 rounded-full px-3 py-1 text-sm font-semibold text-gray-50 mr-2 mb-2">{card.category}</span>
-                }
-                {card.category==='Planning' &&
-                    <span class="inline-block bg-orange-700 rounded-full px-3 py-1 text-sm font-semibold text-gray-50 mr-2 mb-2">{card.category}</span>
-                }
+        <div className="w-72 m-4 h-max bg-gray-50 rounded-lg overflow-hidden">
+            <div className="flex justify-start ml-4 mt-4 mb-2">
+                {card.tags.map((item,index) => {
+                    if(item==='Research'){
+                        return (<span key={index} class="inline-block bg-blue-500 rounded px-3 py-1 text-sm font-semibold text-gray-50 mr-2 mb-2">{item}</span>);
+                    }
+                    if(item==='Design'){
+                        return (<span key={index} class="inline-block bg-violet-700 rounded px-3 py-1 text-sm font-semibold text-gray-50 mr-2 mb-2">{item}</span>);
+                    }
+                    if(item==='Content'){
+                        return (<span key={index} class="inline-block bg-amber-500 rounded px-3 py-1 text-sm font-semibold text-gray-50 mr-2 mb-2">{item}</span>);
+                    }
+                    if(item==='Planning'){
+                        return (<span key={index} class="inline-block bg-orange-700 rounded px-3 py-1 text-sm font-semibold text-gray-50 mr-2 mb-2">{item}</span>);
+                    }
+                })}
             </div>
             {card.imgUrl &&
                 <div className="flex w-full h-28 justify-center">
-                    <img className="w-56 h-28 rounded" src={card.imgUrl} alt="" />
+                    <img className="w-64 h-32 rounded" src={card.imgUrl} alt="" />
                 </div>
             }
             {card.imgUrl &&
-                <div className="px-5 pt-4">
-                    <div className="font-bold text-base mb-1">{card.heading}</div>
-                    <p className="text-gray-700 text-base">{card.desc}</p>
+                <div className="px-5 pt-4 mt-4">
+                    <div className="font-medium font-body text-base mb-1">{card.heading}</div>
+                    <p className="text-gray-500 font-body text-base">{card.desc}</p>
                 </div>
             }
             {!card.imgUrl &&
                 <div className="px-5">
-                    <div className="font-bold text-base mb-1">{card.heading}</div>
-                    <p className="text-gray-700 text-base">{card.desc}</p>
+                    <div className="font-medium font-body text-base mb-1">{card.heading}</div>
+                    <p className="font-body text-gray-500 text-base">{card.desc}</p>
                 </div>
             }
             <div className="flex justify-between px-5 pt-3">
             {/* <span class="border-2 border-gray-200 rounded px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2">{card.date}</span> */}
-            <div class="rounded w-32 py-1 text-sm font-semibold text-gray-900 mb-2">
-                <DatePicker defaultValue={dayjs(card.date, dateFormat)} format={dateFormat} />
+            <div class="rounded w-32 py-1 text-sm font-medium font-body text-gray-900 mb-2">
+                <DatePicker defaultValue={dayjs(card.date, dateFormat)} format={dateFormat2} />
             </div>
             {card.priority==='High' &&
                 <span class="bg-red-200 flex place-items-center rounded-full w-12 h-6 text-sm font-semibold text-red-700 px-2 mt-2">{card.priority}</span>
