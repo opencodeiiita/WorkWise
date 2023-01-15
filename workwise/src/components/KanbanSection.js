7f335ae0b5f30315866da94d053728b8cfa4df6d
Pranav TIwari <pranavt150@gmail.com>
2023-01-15 19:18:53 +0530

    added local storage to cards

--- workwise/src/components/KanbanSection.js
+++ workwise/src/components/KanbanSection.js
@@ -1,30 +1,104 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Cards from "./Cards";
-import { DragDropContext } from "react-beautiful-dnd";
-import Columns from "./ColumnsList";
-import ColumnsList from "./ColumnsList";
-import Card from "./Card";
-import { Button, Modal } from "antd";
-import Column from "./Column";
-// import Kanban from "../pages/kanban";
+import { DragDropContext } from "react-beautiful-dnd";  
 
 const KanbanSection = () => {
   const params = useParams();
-   
-  //Example on how to save header section of kanban
-  // console.log(params.section);
-  // localStorage.setItem(
-  //   params.section,
-  //   JSON.stringify({
-  //     title: "test",
-  //     description: "text",
-  //     others: "no", 
-  //   })
-  // );ui
-  // console.log(JSON.parse(localStorage.getItem(params.section)));
 
-  let cardsData1 = [
+  let cardsData = [...cards];
+
+  const [elements, setElements] = useState(cardsData);
+  useEffect(() => {
+    if (localStorage.getItem(params.section) === null)
+      localStorage.setItem(
+        params.section,
+        JSON.stringify({
+          title: "test",
+          description: "text",
+          others: "no",
+          data: [...cardsData],
+        })
+      );
+    else {
+      let data = JSON.parse(localStorage.getItem(params.section));
+      setElements(data.data[0]);
+    }
+  }, []);
+  const removeFromList = (list, index) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(index, 1);
+    return [removed, result];
+  };
+
+  const addToList = (list, index, element) => {
+    const result = Array.from(list);
+    result.splice(index, 0, element);
+    return result;
+  };
+
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+    const listCopy = { ...elements };
+    const sourceList = listCopy[result.source.droppableId];
+    const [removedElement, newSourceList] = removeFromList(
+      sourceList,
+      result.source.index
+    );
+    listCopy[result.source.droppableId] = newSourceList;
+    const destinationList = listCopy[result.destination.droppableId];
+    listCopy[result.destination.droppableId] = addToList(
+      destinationList,
+      result.destination.index,
+      removedElement
+    );
+    console.log(listCopy);
+    setElements(listCopy);
+    localStorage.setItem(
+      params.section,
+      JSON.stringify({
+        title: "test",
+        description: "text",
+        others: "no",
+        data: [listCopy],
+      })
+    );
+  };
+
+  return (
+    <>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className=" overflow-auto bg-gray-200 h-[100vh] w-[max(calc(100%-300px),67vw)] absolute right-0">
+          {params.section}
+          <div className="flex flex-row flex-wrap characters">
+            <div className="flex flex-row flex-wrap flex-1">
+              <div className="flex-1">
+                <Cards id={0} data={elements[0]} />
+              </div>
+              <div className="flex-1 ">
+                <Cards id={1} data={elements[1]} />
+              </div>
+            </div>
+            <div className="flex flex-row flex-wrap flex-1">
+              <div className="flex-1 ">
+                <Cards id={2} data={elements[2]} />
+              </div>
+              <div className="flex-1 ">
+                <Cards id={3} data={elements[3]} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </DragDropContext>
+    </>
+  );
+};
+
+let cards = [
+  //Temporary Cards Data
+  [
     {
       id: "11",
       tags: ["Research", "Content"],
@@ -33,10 +107,9 @@ const KanbanSection = () => {
       desc: "This is the description",
       date: "2022-08-09",
       priority: "High",
-    } 
-  ];
-
-  let cardsData2 = [
+    },
+  ],
+  [
     {
       id: "21",
       tags: ["Research", "Content"],
@@ -62,9 +135,8 @@ const KanbanSection = () => {
       date: "2012-03-04",
       priority: "Low",
     },
-  ];
-
-  let cardsData3 = [
+  ],
+  [
     {
       id: "31",
       tags: ["Research", "Content"],
@@ -90,9 +162,9 @@ const KanbanSection = () => {
       date: "2012-03-04",
       priority: "Low",
     },
-  ];
+  ],
 
-  let cardsData4 = [
+  [
     {
       id: "41",
       tags: ["Research", "Content"],
@@ -118,78 +190,8 @@ const KanbanSection = () => {
       date: "2012-03-04",
       priority: "Low",
     },
-  ];
-  let cardsData = [cardsData1, cardsData2, cardsData3, cardsData4];
-
-  const [elements, setElements] = useState(cardsData);
-
-  const removeFromList = (list, index) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(index, 1);
-    return [removed, result];
-  };
-
-  const addToList = (list, index, element) => {
-    const result = Array.from(list);
-    // console.log(result);
-    result.splice(index, 0, element);
-    // console.log(result);
-    return result;
-  };
-
-  const onDragEnd = (result) => {
-    if (!result.destination) {
-      return;
-    }
-    const listCopy = { ...elements };
-    // console.log(listCopy);
-    const sourceList = listCopy[result.source.droppableId];
-    // console.log(sourceList);
-    const [removedElement, newSourceList] = removeFromList(
-      sourceList,
-      result.source.index
-    );
-    // console.log(newSourceList);
-
-    listCopy[result.source.droppableId] = newSourceList;
-    const destinationList = listCopy[result.destination.droppableId];
-    // console.log(destinationList);
-    listCopy[result.destination.droppableId] = addToList(
-      destinationList,
-      result.destination.index,
-      removedElement
-    );
-    console.log(listCopy);
-    setElements(listCopy);
-  };
-
-  return (
-		<>
-			<DragDropContext onDragEnd={onDragEnd}>
-				<div className=" overflow-auto bg-gray-200 h-[100vh] w-[max(calc(100%-300px),67vw)] absolute right-0">
-					{params.section}
-					<div className="flex flex-row flex-wrap characters">
-						<div className="flex flex-row flex-wrap flex-1">
-							<div className="flex-1">
-								<Cards id={0} data={elements[0]} />
-							</div>
-							<div className="flex-1 ">
-								<Cards id={1} data={elements[1]} />
-							</div>
-						</div>
-						<div className="flex flex-row flex-wrap flex-1">
-							<div className="flex-1 ">
-								<Cards id={2} data={elements[2]} />
-							</div>
-							<div className="flex-1 ">
-								<Cards id={3} data={elements[3]} />
-							</div>
-						</div>
-					</div>
-				</div>
-			</DragDropContext>
-		</>
-	);
-};
+  ],
+  //Temporary cards data ends here
+];
 
 export default KanbanSection;
