919317dbd41e21dc8561e31b99f254491c29d902
atharva-gadekar <atharvagadekar@gmail.com>
2023-06-24 04:58:36 +0530

    Pomodoro Stage 1

--- workwise/src/components/Time.js
+++ workwise/src/components/Time.js
@@ -1,23 +1,131 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { CiTimer } from "react-icons/ci";
+import { FaRegStopCircle } from "react-icons/fa";
 
 export default function Time() {
-  const [time, setTime] = useState('');
+	const [time, setTime] = useState("");
+	const [timer, setTimer] = useState(25 * 60);
+	const [showTime, setShowTime] = useState(true);
+	const [hovered, setHovered] = useState(false);
+	const [isRunning, setIsRunning] = useState(false);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      let today = new Date();
-      let hrs = addzero(today.getHours());
-      let min = addzero(today.getMinutes());
-      // let sec = addzero(today.getSeconds());
-      setTime(`${hrs}:${min}`);
-    }, 1000);
+	const startTimer = () => {
+		setIsRunning(true);
+	};
 
-    return () => clearInterval(interval);
-  }, []);
+	const endTimer = () => {
+		setIsRunning(false);
+	};
 
-  return <div className='text-9xl text-white font-bold drop-shadow-[0_0_10px_black]'>{time}</div>;
+	useEffect(() => {
+		if (showTime) {
+			const interval = setInterval(() => {
+				let today = new Date();
+				let hrs = addzero(today.getHours());
+				let min = addzero(today.getMinutes());
+				setTime(`${hrs}:${min}`);
+			}, 1000);
+			return () => clearInterval(interval);
+		} else {
+			if (isRunning) {
+				// If the timer is running, decrement it by one second every second
+				const interval = setInterval(() => {
+					setTimer((prevTimer) => prevTimer - 1);
+				}, 1000);
+				// If the timer reaches zero, stop it and reset it
+				if (timer === 0) {
+					clearInterval(interval);
+					setTimer(25 * 60);
+					setIsRunning(false);
+				}
+				return () => clearInterval(interval);
+			} else {
+				// If the timer is not running, do not change it
+				setTime("25:00");
+			}
+		}
+	}, [showTime, isRunning, timer]);
+
+	// Convert the timer value in seconds to a string in minutes and seconds format
+	const formatTimer = (timer) => {
+		let min = addzero(Math.floor(timer / 60));
+		let sec = addzero(timer % 60);
+		return `${min}:${sec}`;
+	};
+
+	return (
+		<>
+			{showTime && (
+				<>
+					<div className="flex justify-center">
+						<div
+							className="text-9xl w-[40vw] text-white font-bold drop-shadow-[0_0_10px_black]"
+							onMouseEnter={() => {
+								setHovered(true);
+							}}
+							onMouseLeave={() => {
+								setHovered(false);
+							}}
+						>
+							{hovered && (
+								<>
+									<CiTimer
+										onClick={() => {
+											setShowTime(!showTime);
+											setTime("25:00");
+										}}
+										className="text-5xl absolute top-12 right-[33vw] inline text-white"
+									/>
+								</>
+							)}
+							{time}
+						</div>
+					</div>
+				</>
+			)}
+			{!showTime && (
+				<>
+					<>
+						<div className="flex justify-center">
+							<div
+								className="text-9xl w-[40vw] text-white font-bold drop-shadow-[0_0_10px_black]"
+								onMouseEnter={() => {
+									setHovered(true);
+								}}
+								onMouseLeave={() => {
+									setHovered(false);
+								}}
+							>
+								<>
+									{isRunning ? (
+										<FaRegStopCircle
+											onClick={() => {
+												setIsRunning(false);
+												endTimer();
+											}}
+											className="text-5xl absolute top-12 right-[33vw] inline text-white"
+										/>
+									) : (
+										<CiTimer
+											onClick={() => {
+												setIsRunning(true);
+												startTimer();
+											}}
+											className="text-5xl absolute top-12 right-[33vw] inline text-white"
+										/>
+									)}
+								</>
+								{/* Display the formatted timer value */}
+								{formatTimer(timer)}
+							</div>
+						</div>
+					</>
+				</>
+			)}
+		</>
+	);
 }
 
 function addzero(num) {
-  return num < 10 ? `0${num}` : num;
+	return num < 10 ? `0${num}` : num;
 }
--- workwise/src/components/sidebar_item.js
+++ workwise/src/components/sidebar_item.js
@@ -1,62 +1,62 @@
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 export default (props) => {
-   const [counter, setCounter] = useState(0);
-  let textadd = (e) => {
-    if (e.key === "Enter") {
-      let txt = e.target.value;
-      if (txt !== null || txt !== "") {
-        e.target.previousSibling.classList.remove("hidden");
-        e.target.previousSibling.childNodes[1].innerHTML = txt;
-        e.target.className = "hidden";
-        localStorage.setItem(props.keyno, txt);
-        setCounter(counter+1);
-      }
-    }
-  };
-  let select = (e) => {
-    e.target.nextSibling.classList.add("bg-gray-200");
-    e.target.nextSibling.classList.add("text-gray-800");
-  };
-  let deselect = (e) => {
-    e.target.nextSibling.classList.remove("bg-gray-200");
-    e.target.nextSibling.classList.remove("text-gray-800");
-  };
-  return (
-    <div className="h-10 text-[.5em] relative flex items-center text-gray-400">
-      <div className=" h-full border-[1px] ml-[0.58rem] border-gray-300"></div>
-      <div className="w-4 mr-2 border-[1px] border-gray-300"> </div>
-      <label
-        className={`${
-          localStorage.getItem(props.keyno) == null ? "hidden" : ""
-        } flex`}
-      >
-        <input
-          type="radio"
-          className={`cursor-pointer 
+	const [counter, setCounter] = useState(0);
+	let textadd = (e) => {
+		if (e.key === "Enter") {
+			let txt = e.target.value;
+			if (txt !== null || txt !== "") {
+				e.target.previousSibling.classList.remove("hidden");
+				e.target.previousSibling.childNodes[1].innerHTML = txt;
+				e.target.className = "hidden";
+				localStorage.setItem(props.keyno, txt);
+				setCounter(counter + 1);
+			}
+		}
+	};
+	let select = (e) => {
+		e.target.nextSibling.classList.add("bg-gray-200");
+		e.target.nextSibling.classList.add("text-gray-800");
+	};
+	let deselect = (e) => {
+		e.target.nextSibling.classList.remove("bg-gray-200");
+		e.target.nextSibling.classList.remove("text-gray-800");
+	};
+	return (
+		<div className="h-10 text-[.5em] relative flex items-center text-gray-400">
+			<div className=" h-full border-[1px] ml-[0.58rem] border-gray-300"></div>
+			<div className="w-4 mr-2 border-[1px] border-gray-300"> </div>
+			<label
+				className={`${
+					localStorage.getItem(props.keyno) == null ? "hidden" : ""
+				} flex`}
+			>
+				<input
+					type="radio"
+					className={`cursor-pointer 
             opacity-100 w-0 `}
-          name="projects"
-          onFocus={select}
-          onBlur={deselect}
-        />
-        <Link to={`${localStorage.getItem(props.keyno)}`}>
-          <div
-            className={`${
-              props.select ? "bg-gray-200 text-gray-800" : ""
-            } cursor-pointer px-3 py-1 rounded-xl transition hover:bg-gray-200 hover:text-gray-800`}
-          >
-            {localStorage.getItem(props.keyno)}
-          </div>
-        </Link>
-      </label>
-      <input
-        type="text"
-        autoFocus
-        className={`${
-          localStorage.getItem(props.keyno) === null ? "" : "hidden"
-        } bg-gray-100 border-b-[1px] border-black outline-none w-4/5 text-black p-1 pl-2`}
-        onKeyDown={textadd}
-      />
-    </div>
-  );
+					name="projects"
+					onFocus={select}
+					onBlur={deselect}
+				/>
+				<Link to={`${localStorage.getItem(props.keyno)}`}>
+					<div
+						className={`${
+							props.select ? "bg-gray-200 text-gray-800" : ""
+						} cursor-pointer px-3 py-1 rounded-xl transition hover:bg-gray-200 hover:text-gray-800`}
+					>
+						{localStorage.getItem(props.keyno)}
+					</div>
+				</Link>
+			</label>
+			<input
+				type="text"
+				autoFocus
+				className={`${
+					localStorage.getItem(props.keyno) === null ? "" : "hidden"
+				} bg-gray-100 border-b-[1px] border-black outline-none w-4/5 text-black p-1 pl-2`}
+				onKeyDown={textadd}
+			/>
+		</div>
+	);
 };
